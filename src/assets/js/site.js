48f8f4ad3b6c8d0c03497e67fc9c064f801a8fc1
Yev <yev@Yevs-MacBook-Air.local>
2025-12-12 11:36:44 +0200

    Update site styles and menu for mob 3

---
+++ src/assets/js/site.js
@@ -0,0 +1,6 @@
+  const btn = document.getElementById("navbar");
+  const menu = document.getElementById("navbar");
+
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("show");
+  });
